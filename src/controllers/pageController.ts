c16c51383b482afe314925d89672caef63efd434
Saulo Melo <saulojmc@id.uff.br>
2021-07-19 11:45:15 -0300

    base pronta

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,37 +1,60 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const home = (req: Request, res: Response) => {
+
+    let list = Pet.getAll();
+
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+
+    let list = Pet.getFromtype('dog');
+
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
+        },
+        list
     });
 }
 
 export const cats = (req: Request, res: Response) => {
+
+    let list = Pet.getFromtype('cat');
+
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+
+    let list = Pet.getFromtype('fish');
+
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('página não encontrada!');
+    res.render('pages/404');
 });
 
 //Rotas
